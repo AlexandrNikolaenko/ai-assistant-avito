b4bad6127e43cce256ffcce8448132fe66b19ad3
Александр <nikol.alex06@mail.ru>
2026-03-25 23:25:34 +0300

    feat: add css file

---
+++ frontend/src/shared/constants/params.ts
@@ -0,0 +1,27 @@
+export const paramsRu: {
+  brand: string;
+  model: string;
+  yearOfManufacture: string;
+  transmission: string;
+  mileage: string;
+  enginePower: string;
+  type: string;
+  address: string;
+  area: string;
+  floor: string;
+  condition: string;
+  color: string;
+} = {
+  brand: 'Брэнд',
+  model: 'Модель',
+  yearOfManufacture: 'Год производства',
+  transmission: 'КПП',
+  mileage: 'Пробег',
+  enginePower: 'Мощность двигателя',
+  type: 'Тип',
+  address: 'Адресс',
+  area: 'Район',
+  floor: 'Этаж',
+  condition: 'Состояние',
+  color: 'Цвет'
+}
